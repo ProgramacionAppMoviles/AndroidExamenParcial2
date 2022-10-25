5acfaefd177657f951681910d0fde62c489c0840
JuCaAlonsoBravo <jucaalonsobravo@hotmail.com>
2022-10-25 10:26:21 -0500

    Diseño para dos pantallas (layouts)

---
+++ app/src/main/java/com/jcab/examen2domovil/mostrar_curp.java
@@ -0,0 +1,14 @@
+package com.jcab.examen2domovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mostrar_curp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_curp);
+    }
+}
